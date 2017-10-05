f3a354c8c45384b08a1350e0525c98590fdbf427
riemeri <32551363+riemeri@users.noreply.github.com>
2017-10-05 11:09:37 -0700

    Intial Commit

---
+++ app/src/main/java/com/bignerdranch/ianr/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.ianr.geoquiz;
+
+/**
+ * Created by ianri on 9/27/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
